616de3ea136c6ba495c416266d40e6c2ff7cf809
touil <touilmustapha534@gmail.com>
2019-03-06 16:36:39 +0100

    modification exercices

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Palindrome {
 
     public static void main(String[] args){
-String mot1="LavaL";
+
+String mot1="pirates";
 StringBuffer mot2= new StringBuffer(mot1);
 mot2.reverse();
 
         System.out.println(mot1);
         System.out.println(mot2);
-
 
         if (mot1.equals(mot2)){
             System.out.println("ce mot est un palindrome");
--- src/inverserChaine.java
+++ src/inverserChaine.java
@@ -1,6 +1,6 @@
 public class inverserChaine {
     public static void main(String[] args){
-        String mot1="pirates";
+        String mot1="parisien";
         StringBuffer mot2= new StringBuffer(mot1);
         mot2.reverse();
 
